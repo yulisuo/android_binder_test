65ca4d5182599fafbb150686a8879172b6908da6
lsyu <lsyu@mobvoi.com>
2017-01-20 16:05:24 +0800

    add sp and wp

--- Test.cpp
+++ Test.cpp
@@ -6,4 +6,13 @@ void TestRef::setNum(int n){
     num = n;
 }
 
+int main(){
+    TestRef* pT = new TestRef;
+    {
+        sp<TestRef> spT(pT);
+        wp<TestRef> wpT(spT);
+    }
+    return 0;
+}
+
 }
